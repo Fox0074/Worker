68deadf90f24bef2d98de125d11d8fc07a51179d
Yury <xismatulin.ru@yandex.ru>
2018-03-14 13:54:33 +0500

    Интервал HashMiner

--- FastHashMiner/FastHashMiner/Loading.cs
+++ FastHashMiner/FastHashMiner/Loading.cs
@@ -103,7 +103,7 @@ namespace FastHashMiner
                 Ping myPing = new Ping();
                 String host = hostName;
                 byte[] buffer = new byte[32];
-                int timeout = 500;
+                int timeout = 1000;
                 PingOptions pingOptions = new PingOptions();
                 PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
                 return (reply.Status == IPStatus.Success);
