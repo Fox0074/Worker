112a6bdbfd4408673350815c6e37b4bae86a9bab
Yury <xismatulin.ru@yandex.ru>
2018-03-29 21:03:14 +0500

    Продумка архитектуры

---
+++ ServerWorker/Commands/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerWorker.Commands
+{
+    public enum SendCommands { };
+    public enum InnerCommands {  };
+    public class Unit
+    {
+        public string command;
+        public List<string> parametrs = new List<string>();
+    }
+}
