ad14fc83269e93bc4c188d16766cc8efc4e7b22d
DESKTOP-FPJQBO8\Fox <Xismatulin.ru@yandex.ru>
2019-07-12 23:38:15 +0500

    NewCppTorrentMask

--- Masks/torrent/torrent/torrent/resource.h
+++ Masks/torrent/torrent/torrent/resource.h
@@ -2,18 +2,14 @@
 // Включаемый файл, созданный в Microsoft Visual C++.
 // Используется torrent.rc
 //
-#define IDR_FILE1                       101
-#define IDI_ICON1                       103
-#define IDR_FILE2                       104
-#define IDR_FILE3                       105
-#define IDR_FILE4                       106
-#define IDR_FILE5                       107
+#define IDI_ICON1                       101
+#define IDR_FILE1                       102
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
